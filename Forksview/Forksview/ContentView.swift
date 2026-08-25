a4ca75d3257da027dc2c9962e60840b0ee7d55fe
Michael Buckingham <michaelbuckingham74@gmail.com>
2026-08-24 21:49:02 -0700

    Initialize Forksview macOS app

---
+++ Forksview/Forksview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Forksview
+//
+//  Created by Michael Buckingham on 8/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Forksview/Forksview/ForksviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForksviewApp.swift
+//  Forksview
+//
+//  Created by Michael Buckingham on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct ForksviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
